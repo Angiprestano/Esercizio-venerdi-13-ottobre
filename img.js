960b40701215fbe498d9df469722410211bfb9f0
Angela Prestano <angiprestano99@gmail.com>
2023-10-13 22:52:54 +0200

    modiche in corso..

--- img.js
+++ img.js
@@ -1,10 +1,1 @@
-const image = document.getElementsByTagName("image");
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
 
-  const imageInput = document.getElementById("image");
-
-  const newProduct = {
-    image: imageInput.value,
-  };
-});
